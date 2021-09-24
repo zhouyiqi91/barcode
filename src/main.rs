6f7f70e68e153f2153f873e21f2645202b6a06e9
zhouyiqi91 <386346852@qq.com>
2021-09-24 06:52:13 +0000

    cli

---
+++ src/main.rs
@@ -0,0 +1,77 @@
+use std::fs;
+use std::io::{self, Write};
+
+use log::{info, warn};
+use simplelog::*;
+use structopt::StructOpt;
+use anyhow::{Context, Result};
+
+/// cli
+#[derive(StructOpt)]
+struct Cli {
+    /// fastq 1
+    #[structopt(long = "fq1")]
+    fq1: String,
+    /// fastq2
+    #[structopt(long = "fq2")]
+    fq2: String,
+}
+
+
+fn main() -> Result<()>{
+
+    // logger
+    CombinedLogger::init(
+        vec![
+            TermLogger::new(LevelFilter::Info, Config::default(), TerminalMode::Mixed, ColorChoice::Auto),
+            WriteLogger::new(LevelFilter::Info, Config::default(), fs::File::create("my_rust_binary.log").unwrap()),
+        ]
+    ).unwrap();
+
+    //IO
+    let out_fq1_file = "out_1.fq";
+    let out_fq1 = fs::File::create(out_fq1_file).unwrap();
+    let mut handle = io::BufWriter::new(out_fq1);
+    let args = Cli::from_args();
+
+    info!("read fastq1");
+    warn!("warn you!!!!!");
+    let fq1 = fs::read_to_string(&args.fq1)
+        .with_context(|| format!("could not read file `{}`", &args.fq1))?;
+    write_fastq(fq1, &mut handle);
+    
+    Ok(())
+}
+
+
+fn write_fastq<W>(content: String, mut writer: W) 
+where W: std::io::Write
+{
+    for line in content.lines() {
+        writer.write_all(line.as_bytes()).unwrap();
+        writer.write(&['\n' as u8]).unwrap();
+    }
+}
+
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_write_fastq() {
+        let mut writer = Vec::new();
+        let contents = String::from("contents");
+        write_fastq(contents, &mut writer);
+        println!("{:?}", writer);
+        
+        //let output = String::from_utf8(writer).unwrap();
+        assert_eq!(writer, b"contents\n");
+    }
+
+    #[test]
+    fn vec_test(){
+        let mut writer = Vec::new();
+        writer.push('a' as u8);
+        assert_eq!(writer, b"a");
+    }
+}
+
---
+++ tests/cli.rs
@@ -0,0 +1,19 @@
+use assert_cmd::prelude::*; // Add methods on commands
+use predicates::prelude::*; // Used for writing assertions
+use std::process::Command; // Run programs
+
+#[test]
+fn file_doesnt_exist() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("barcode")?;
+
+    cmd.arg("--fq1")
+        .arg("fq1")
+        .arg("--fq2")
+        .arg("fq2");
+    
+    cmd.assert()
+        .failure()
+        .stderr(predicate::str::contains("No such file or directory"));
+
+    Ok(())
+}
